85d0bd484709261bf05021dcd83fa843980c135e
Ricky Wong <wricky-t@student.42kl.edu.my>
2022-11-01 16:22:34 +0800

    added new sprites

--- assets/sprites.h
+++ assets/sprites.h
@@ -6,7 +6,7 @@
 /*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 18:25:39 by wricky-t          #+#    #+#             */
-/*   Updated: 2022/10/25 16:43:06 by wricky-t         ###   ########.fr       */
+/*   Updated: 2022/11/01 15:32:25 by wricky-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,19 @@
 # define PLAYERR2 "assets/player/player_idle_r2.xpm"
 # define PLAYERR3 "assets/player/player_idle_r3.xpm"
 # define PLAYERR4 "assets/player/player_idle_r4.xpm"
-# define SKELY1 "assets/skeleton/skeleton_idle1.xpm"
-# define SKELY2 "assets/skeleton/skeleton_idle2.xpm"
-# define SKELY3 "assets/skeleton/skeleton_idle3.xpm"
+# define SKELYL1 "assets/skeleton/skeleton_idle_l1.xpm"
+# define SKELYL2 "assets/skeleton/skeleton_idle_l2.xpm"
+# define SKELYL3 "assets/skeleton/skeleton_idle_l3.xpm"
+# define SKELYR1 "assets/skeleton/skeleton_idle_r1.xpm"
+# define SKELYR2 "assets/skeleton/skeleton_idle_r2.xpm"
+# define SKELYR3 "assets/skeleton/skeleton_idle_r3.xpm"
+# define SCARED1 "assets/skeleton/scared1.xpm"
+# define SCARED2 "assets/skeleton/scared2.xpm"
+# define SCARED3 "assets/skeleton/scared3.xpm"
+# define SCARED4 "assets/skeleton/scared4.xpm"
+# define SCARED5 "assets/skeleton/scared5.xpm"
+# define SCARED6 "assets/skeleton/scared6.xpm"
+# define SCARED7 "assets/skeleton/scared7.xpm"
 # define GHOST1 "assets/ghost/ghost_idle1.xpm"
 # define GHOST2 "assets/ghost/ghost_idle2.xpm"
 # define GHOST3 "assets/ghost/ghost_idle3.xpm"
@@ -60,5 +70,15 @@
 # define FLOOR "assets/other/floor.xpm"
 # define BLOCK "assets/other/block.xpm"
 # define BG "assets/other/bg.xpm"
+# define ZERO "assets/numbers/zero.xpm"
+# define ONE "assets/numbers/one.xpm"
+# define TWO "assets/numbers/two.xpm"
+# define THREE "assets/numbers/three.xpm"
+# define FOUR "assets/numbers/four.xpm"
+# define FIVE "assets/numbers/five.xpm"
+# define SIX "assets/numbers/six.xpm"
+# define SEVEN "assets/numbers/seven.xpm"
+# define EIGHT "assets/numbers/eight.xpm"
+# define NINE "assets/numbers/nine.xpm"
 
 #endif
